9a31aededed02e71033ccb49300188f08bb80beb
Grace Celine Florensia <gflorensia@techfak.uni-bielefeld.de>
2024-11-02 22:47:50 +0100

    fixes #80079

--- app/src/main/java/de/techfak/se/gflorensia/MainActivity.java
+++ app/src/main/java/de/techfak/se/gflorensia/MainActivity.java
@@ -1,6 +1,10 @@
 package de.techfak.se.gflorensia;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.OptionalInt;
+import java.util.Set;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.res.AssetManager;
 import android.os.Bundle;
@@ -12,6 +16,7 @@ import java.lang.Math;
 import java.math.BigDecimal;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,14 +25,11 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 /**
  * This is the MainActivity that is executed when the app is started.
  */
 public class MainActivity extends AppCompatActivity {
-
     String TAG = "GeoJson";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,16 +38,13 @@ public class MainActivity extends AppCompatActivity {
         String path = "maps";
         Map<String,PointOfInterest> poiMap = new HashMap<>();
         List<String> files = getFolder(path);
-
         if (files != null && !files.isEmpty()) {
             boolean geoJsonFound = false;
-
             // Loop through the files and filter .geojson files
             for (String file : files) {
                 if (file.endsWith(".geojson")) {
                     Log.d(TAG, "GeoJson file found: " + file);
                     geoJsonFound = true;
-
                     String jsonContent = getJsonContent(path + "/" + file);
                     try {
                         poiMap = extractPOI(jsonContent);
@@ -53,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
-
                 if (file.equals("small.geojson")) {
                     String smallJson = getJsonContent(path + "/" + "small.geojson");
                     try {
@@ -62,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
                         throw new RuntimeException(e);
                     }
                 }
-
                 if (file.equals("corrupted.geojson")){
                     String corruptedJson = getJsonContent(path + "/" + "corrupted.geojson");
                     try {
@@ -73,18 +70,15 @@ public class MainActivity extends AppCompatActivity {
                     findIsolatedPOI(poiMap);
                 }
             }
-
             // If no .geojson files were found
             if (!geoJsonFound) {
                 Log.d(TAG, "No GeoJson files found in folder: " + path);
             }
-
         } else {
             // Log if the folder is empty or does not exist
             Log.d(TAG, "The folder " + path + " is empty or does not exist.");
         }
     }
-
     /**
      * Returns a list with all filenames of a folder inside the assets.
      * Pay attention that a empty list is returned if the folder does not exists.
@@ -106,7 +100,6 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
     }
-
     /**
      * Return the InputStream of a file inside the assets.
      * Pay attention that the InputStream must be closed after use.
@@ -124,50 +117,39 @@ public class MainActivity extends AppCompatActivity {
             return null;
         }
     }
-
     public String getJsonContent(String filePath){
         BufferedReader br = new BufferedReader(
                 new InputStreamReader(getFileInputStream(filePath))
         );
         return br.lines().collect(Collectors.joining());
     }
-
     public Map<String,PointOfInterest> extractPOI(String jsonContent) throws JSONException {
-
         ObjectMapper om = new ObjectMapper();
         Map<String,PointOfInterest> poiMap = new HashMap<>();
-
         try {
             JsonNode root = om.readTree(jsonContent);
-
             for (JsonNode jn : root.get("features")) {
                 String featureType = jn.get("geometry").get("type").asText();
                 if (featureType.equals("Point")) {
                     String name = jn.get("properties").get("name").asText();
                     JsonNode coordinates = jn.get("geometry").get("coordinates");
-
                     // Extract longitude and latitude from the coordinates array
                     BigDecimal latitude = jn.get("geometry").get("coordinates").get(0).decimalValue();
                     BigDecimal longitude = jn.get("geometry").get("coordinates").get(1).decimalValue();
-
                     // Create a new PointOfInterest object
                     PointOfInterest poi = new PointOfInterest(name, latitude, longitude);
                     poiMap.put(name, poi);
-
                     // Optionally log the POI
                     Log.i("POI",poi.describePOI());
                 }
-
             }
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
         return poiMap;
     }
-
     public void createConnections(String jsonContent, Map<String, PointOfInterest> poiMap) throws IOException, JSONException {
         ObjectMapper om = new ObjectMapper();
-
         // Handle connections (if any)
         JsonNode root = om.readTree(jsonContent);
         for (JsonNode jn : root.get("features")) {
@@ -175,12 +157,10 @@ public class MainActivity extends AppCompatActivity {
             if (featureType.equals("LineString")) {
                 JsonNode destination = jn.get("properties").get("routePoints");
                 JsonNode transport = jn.get("properties").get("typeId");
-
                 List<String> typeId = new ArrayList<>();
                 for (JsonNode idNode : transport) {
                     typeId.add(idNode.asText());
                 }
-
                 for (String transportMode : typeId) {
                     if (poiMap.containsKey(destination.get("p1").asText())) {
                         PointOfInterest poi = poiMap.get(destination.get("p1").asText());
@@ -189,7 +169,6 @@ public class MainActivity extends AppCompatActivity {
                         poi.addConnection(connection);
                         Log.i("Connection", connection.describeConnection());
                     }
-
                     if (poiMap.containsKey(destination.get("p2").asText())) {
                         PointOfInterest poi = poiMap.get(destination.get("p2").asText());
                         Connection connection = new Connection(transportMode, poi);
@@ -201,7 +180,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
     public List<PointOfInterest> findIsolatedPOI(Map<String,PointOfInterest> poiMap){
         List<PointOfInterest> isolatedPOIs = new ArrayList<>();
         for (PointOfInterest poi : poiMap.values()) {
--- app/src/main/java/de/techfak/se/gflorensia/PointOfInterest.java
+++ app/src/main/java/de/techfak/se/gflorensia/PointOfInterest.java
@@ -1,15 +1,17 @@
 package de.techfak.se.gflorensia;
+import java.lang.Math;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.List;
 import java.math.BigDecimal;
 import java.util.Objects;
-
 class PointOfInterest {
     private String name;
     private BigDecimal latitude;
     private BigDecimal longitude;
     private List<Connection> connections = new ArrayList<>();
-
     public PointOfInterest(String name, BigDecimal latitude, BigDecimal longitude) {
         this.name = name;
         this.latitude = latitude;
@@ -18,30 +20,23 @@ class PointOfInterest {
     public String describePOI() {
         return "Name: " + name + ", Lat: " + latitude + ", Long: " + longitude;
     }
-
     public String getName(){
         return name;
     }
-
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PointOfInterest that = (PointOfInterest) o;
         return name.equals(that.name) && longitude.equals(that.longitude) && latitude.equals(that.latitude);
     }
-
     @Override
     public int hashCode() {
         return Objects.hash(latitude, longitude);
     }
-
     public void addConnection(Connection connection) {
         connections.add(connection);
     }
-
     public List<Connection> getConnections() {
         return connections;
     }
-
-
 }
