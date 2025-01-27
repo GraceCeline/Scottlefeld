7c881c42821cbf16a6e6d4383a089f6741f13786
Grace Celine Florensia <gflorensia@techfak.uni-bielefeld.de>
2025-01-27 15:59:48 +0100

    fixes #84401 @0:15 : fixes test cases for acceptance criteria 2.1

--- app/src/androidTest/java/de/techfak/se/gflorensia/ValidateMoveTest.java
+++ app/src/androidTest/java/de/techfak/se/gflorensia/ValidateMoveTest.java
@@ -4,10 +4,11 @@ import org.junit.Before;
 import org.junit.Test;
 import java.math.BigDecimal;
 
-import de.techfak.se.gflorensia.controller.StartActivity;
+import de.techfak.gse24.botlib.PlayerFactory;
 import de.techfak.se.gflorensia.model.Connection;
 import de.techfak.se.gflorensia.model.Player;
 import de.techfak.se.gflorensia.model.PointOfInterest;
+import de.techfak.se.gflorensia.model.GameModel;
 
 public class ValidateMoveTest extends TestCase {
     private final String BUS = "bus";
@@ -29,6 +30,7 @@ public class ValidateMoveTest extends TestCase {
             BigDecimal.valueOf(52.022881),
             BigDecimal.valueOf(8.532977));
     private Player player;
+    GameModel gameModel =  new GameModel();
 
     @Before
     public void setUp() {
@@ -43,38 +45,89 @@ public class ValidateMoveTest extends TestCase {
     }
 
     /**
-    * 1) Test: AC 2.1 - A move is only valid if the destination is different from the current location.
-            *    Equivalence Class: "Different POI selected" vs. "Same POI selected"
-            *    Initial State: Current location and valid destination are distinct.
+     * 2)
+     * Akzeptanzkriterium: (2.1) Zug ist valid, weil ein Zielort wird gewählt, der nicht zu aktuellem Standort identisch ist
+     * <p>
+     * Äquivalenzklasse: Neuer (unidentische zum aktuellen Standort) Standort wird gewählt
+     * <p>
+     * Ausgangszustand:
+     * <br>
+     * Aktueller eigener Standort: Stadthalle Bielefeld
+     * <p>
+     * Benutztes Verkehrsmittel : eScooter
+     * <p>
+     * Aktuelle Ticketanzahl: (Bus:2, Tram:0, eScooter:8)
+     * <p>
+     * Zug: (Zielort: Hauptbahnhof Bielefeld, Stadthalle Bielefeld, Verkehrsmittel: eScooter)
+     * <p>
+     * Erwartete Exception: -
      */
     @Test
     public void testDifferentDestination() {
 
-        boolean moveValid = !currentPOI.equals(destinationValid);
-        assertTrue(moveValid);
+        try {
+            gameModel.validateMove(currentPOI, destinationValid, SCOOTER, player);
+            player.decScooterTickets();
+            assertTrue(true);
+        } catch (InvalidConnectionException | ZeroTicketException e) {
+            fail("Unexpected exception" + e.getMessage());
+        }
     }
 
     /**
-     * 2) Test: AC 2.1 (Positive case) - Destination must be different from current location.
-     *    Equivalence Class: "Same POI selected"
-     *    Initial State: Attempting to move from currentPOI to an identical POI.
+     * 2)
+     * Akzeptanzkriterium: (2.1) Das Ziel ist zu aktuellen Standort identisch (Negative case)
+     * <p>
+     * Äquivalenzklasse: Aktueller Standort gewählt
+     * <p>
+     * Ausgangszustand:
+     * <br>
+     * Aktueller eigener Standort: Hauptbahnhof Bielefeld
+     * <p>
+     * Benutztes Verkehrsmittel : -
+     * <p>
+     * Aktuelle Ticketanzahl: (Bus:2, Tram:0, eScooter:8)
+     * <p>
+     * Zug: -
+     * <p>
+     * Erwartete Exception: InvalidConnectionException
      */
     @Test
     public void testEqualDestination() {
 
-        boolean moveInvalid = currentPOI.equals(destinationInvalid);
-        assertTrue(moveInvalid);
+        try {
+            gameModel.validateMove(currentPOI, destinationInvalid, SCOOTER, player);
+            fail("Should throw Exception because destination is identical to currentPOI.");
+        } catch (InvalidConnectionException e) {
+            assertTrue(true);
+        } catch (Exception | ZeroTicketException e) {
+            fail();
+        }
     }
 
+
     /**
-     * 3) Test: AC 2.2 (Positive Case) - There must be a direct connection from current location to the destination.
-     *    Equivalence Class: "Direct connection exists"
-     *    Initial State: currentPOI -> destinationValid has a bus/tram/scooter connection
+     * 3)
+     * Akzeptanzkriterium: (2.3) Es gibt eine Verbindung vom aktuellen Standort zum Ziel (Positive case)
+     * <p>
+     * Äquivalenzklasse: Direkte Verbindung existiert
+     * <p>
+     * Ausgangszustand:
+     * <br>
+     * Aktueller eigener Standort: Stadthalle Bielefeld
+     * <p>
+     * Benutztes Verkehrsmittel : escooter
+     * <p>
+     * Aktuelle Ticketanzahl: (Bus:2, Tram:0, eScooter:8)
+     * <p>
+     * Zug: (Zielort: Hauptbahnhof Bielefeld, Stadthalle Bielefeld, Verkehrsmittel: eScooter)
+     * <p>
+     * Erwartete Exception: -
      */
     @Test
     public void testConnectionExist() {
         try {
-            StartActivity.validateMove(currentPOI, destinationValid, SCOOTER, player);
+            gameModel.validateMove(currentPOI, destinationValid, SCOOTER, player);
             assertTrue(true);
         } catch (Exception | InvalidConnectionException | ZeroTicketException e) {
             fail("Unexpected exception: " + e.getMessage());
@@ -82,14 +135,27 @@ public class ValidateMoveTest extends TestCase {
     }
 
     /**
-     * 4) Test: AC 2.2 (Negative case) - No direct connection from current location to the destination.
-     *    Equivalence Class: "No direct connection"
-     *    Initial State: currentPOI -> destinationConnectionFail has no connections
+     * 4)
+     * Akzeptanzkriterium: (2.2) Keine Verbindung von aktuellem Standort zum Ziel (Negative case)
+     * <p>
+     * Äquivalenzklasse: Keine Verbindung
+     * <p>
+     * Ausgangszustand:
+     * <br>
+     * Aktueller eigener Standort: -
+     * <p>
+     * Benutztes Verkehrsmittel : -
+     * <p>
+     * Aktuelle Ticketanzahl: (Bus:2, Tram:0, eScooter:8)
+     * <p>
+     * Zug: (Zielort: Hauptbahnhof Bielefeld, Neumarkt, Verkehrsmittel: eScooter)
+     * <p>
+     * Erwartete Exception: InvalidConnectionException
      */
     @Test
     public void testConnectionDoesNotExist() {
         try {
-            StartActivity.validateMove(currentPOI, destinationConnectionFail, SCOOTER, player);
+            gameModel.validateMove(currentPOI, destinationConnectionFail, SCOOTER, player);
             fail("Should throw InvalidConnectionException because there's no direct connection.");
         } catch (InvalidConnectionException e) {
             // Expected
@@ -100,14 +166,27 @@ public class ValidateMoveTest extends TestCase {
     }
 
     /**
-     * 5) Test: AC 2.3 - The selected means of transport is permitted for the connection.
-     *    Equivalence Class: "Connection has the transport mode"
-     *    Initial State: currentPOI -> destinationValid is connected with a scooter
+     * 5)
+     * Akzeptanzkriterium: (2.3) Für die Verbindung ist das benutzte Verkehrsmittel zulässig (Positive case)
+     * <p>
+     * Äquivalenzklasse: Verbindung hat das benutztes Verkehrsmittel
+     * <p>
+     * Ausgangszustand:
+     * <br>
+     * Aktueller eigener Standort: Stadthalle Bielefeld
+     * <p>
+     * Benutztes Verkehrsmittel : escooter
+     * <p>
+     * Aktuelle Ticketanzahl: (Bus:2, Tram:0, eScooter:8)
+     * <p>
+     * Zug: (Zielort: Hauptbahnhof Bielefeld, Stadthalle Bielefeld, Verkehrsmittel: eScooter)
+     * <p>
+     * Erwartete Exception: -
      */
     @Test
     public void testTransportModeExist() {
         try {
-            StartActivity.validateMove(currentPOI, destinationValid, SCOOTER, player);
+            gameModel.validateMove(currentPOI, destinationValid, SCOOTER, player);
             assertTrue(true);
         } catch (Exception | InvalidConnectionException | ZeroTicketException e) {
             fail("Should not throw exception if the transport mode is permitted.");
@@ -115,14 +194,27 @@ public class ValidateMoveTest extends TestCase {
     }
 
     /**
-     * 6) Test: AC 2.3 (Negative case) - The selected means of transport is NOT permitted for the connection.
-     *    Equivalence Class: "Connection doesn't have that transport mode"
-     *    Initial State: currentPOI -> destinationValid is not connected with a bus
+     * 6)
+     * Akzeptanzkriterium: (2.3) Das benutzte Verkehrsmittel ist für den Zug unerlaubt (Negative case)
+     * <p>
+     * Äquivalenzklasse: Verbindung mit falschem Verkehrsmittel
+     * <p>
+     * Ausgangszustand:
+     * <br>
+     * Aktueller eigener Standort: Hauptbahnhof Bielefeld
+     * <p>
+     * Benutztes Verkehrsmittel : -
+     * <p>
+     * Aktuelle Ticketanzahl: (Bus:2, Tram:0, eScooter:8)
+     * <p>
+     * Zug: (Zielort: Hauptbahnhof Bielefeld, Stadthalle Bielefeld, Verkehrsmittel: Bus)
+     * <p>
+     * Erwartete Exception: InvalidConnectionException
      */
     @Test
     public void testTransportModeDoesNotExist() {
         try {
-            StartActivity.validateMove(currentPOI, destinationValid, BUS, player);
+            gameModel.validateMove(currentPOI, destinationValid, BUS, player);
             fail("Should throw InvalidConnectionException");
         } catch (InvalidConnectionException  e) {
             assertTrue(true);
@@ -132,17 +224,31 @@ public class ValidateMoveTest extends TestCase {
     }
 
     /**
-     * 7) Test: AC 2.4 - The detective has at least one ticket for the selected transport mode.
-     *    Equivalence Class: "Enough tickets"
-     *    Initial State: detectivePlayer has bus tickets
+     * 7)
+     * Akzeptanzkriterium: (2.4) Für das benutzte Verkehrsmittel hat der Detective mindestens ein Ticket (Positive case)
+     * <p>
+     * Äquivalenzklasse: Genug Tickets für diesen Zug
+     * <p>
+     * Ausgangszustand:
+     * <br>
+     * Aktueller eigener Standort: Stadthalle Bielefeld
+     * <p>
+     * Benutztes Verkehrsmittel : escooter
+     * <p>
+     * Aktuelle Ticketanzahl: (Bus:2, Tram:0, eScooter:7)
+     * <p>
+     * Zug: (Zielort: Hauptbahnhof Bielefeld, Jahnplatz, Verkehrsmittel: Tram)
+     * <p>
+     * Erwartete Exception: ZeroTicketException
      */
     @Test
     public void testEnoughTickets() {
         try {
-            StartActivity.validateMove(currentPOI,
+            gameModel.validateMove(currentPOI,
                     destinationValid,
                     SCOOTER,
                     player);
+            player.decScooterTickets();
             assertTrue(true);
         } catch (Exception | InvalidConnectionException | ZeroTicketException e) {
             fail("Unexpected exception: " + e.getMessage());
@@ -150,20 +256,33 @@ public class ValidateMoveTest extends TestCase {
     }
 
     /**
-     * 8) Test: AC 2.4 (Negative case) - The detective does NOT have any ticket for the selected transport mode.
-     *    Equivalence Class: "Zero tickets"
-     *    Initial State: detectivePlayer has no scooter tickets
+     * 8)
+     * Akzeptanzkriterium: (2.4) Für das benutzte Verkehrsmittel hat der Detective kein Ticket (Negative case)
+     * <p>
+     * Äquivalenzklasse: Zero Tickets für das Verkehrsmittel
+     * <p>
+     * Ausgangszustand:
+     * <br>
+     * Aktueller eigener Standort: Hauptbahnhof Bielefeld
+     * <p>
+     * Benutztes Verkehrsmittel : tram
+     * <p>
+     * Aktuelle Ticketanzahl: (Bus:2, Tram:0, eScooter:8)
+     * <p>
+     * Zug: (Zielort: Hauptbahnhof Bielefeld, Jahnplatz, Verkehrsmittel: Tram)
+     * <p>
+     * Erwartete Exception: ZeroTicketException
      */
     @Test
     public void testNotEnoughTickets() {
         try {
-            StartActivity.validateMove(
+            gameModel.validateMove(
                     currentPOI,
                     destinationTram,
                     TRAM,
                     player
             );
-            fail("Should throw ZeroTicketException because detective has 0 scooter tickets.");
+            fail("Should throw ZeroTicketException because detective has 0 tram tickets.");
         } catch (ZeroTicketException e) {
             assertTrue(true);
         } catch (Exception | InvalidConnectionException e) {
@@ -173,16 +292,29 @@ public class ValidateMoveTest extends TestCase {
     }
 
     /**
-     * 9) Test: InvalidConnectionException thrown for an invalid connection.
-     *    Equivalence Class: "Invalid connection attempt"
-     *    Initial State: currentPOI -> invalidDestination is not in the connections list
+     * 9)
+     * Akzeptanzkriterium: (3) InvalidConnectionException wird geworfen für ungültige (nicht existierende) Verbindung
+     * <p>
+     * Äquivalenzklasse: Ein Zug ist ungültig, da der aktuelle Standort und das Ziel nicht verbunden sind
+     * <p>
+     * Ausgangszustand:
+     * <br>
+     * Aktueller eigener Standort: Hauptbahnhof Bielefeld
+     *
+     * Benutztes Verkehrsmittel : -
+     *
+     * Aktuelle Ticketanzahl: (Bus:2, Tram:0, eScooter:8)
+     * <p>
+     * Zug: (Zielort: Hauptbahnhof Bielefeld, Neumarkt, Verkehrsmittel: Bus)
+     * <p>
+     * Erwartete Exception: InvalidConnectionException
      */
     @Test
     public void testInvalidConnectionExceptionThrown() {
         try {
-            StartActivity.validateMove(
+            gameModel.validateMove(
                     currentPOI,
-                    destinationInvalid,
+                    destinationConnectionFail,
                     "bus",
                     player
             );
@@ -195,20 +327,33 @@ public class ValidateMoveTest extends TestCase {
     }
 
     /**
-     * 10) Test: ZeroTicketException thrown for an unavailable ticket type.
-     *     Equivalence Class: "No tickets for the selected mode"
-     *     Initial State: detectivePlayer has 0 tram tickets
+     * Akzeptanzkriterium: (4) ZeroTicketException wird geworfen für einen Zug ohne verfügbare Ticket für das genutzte Verkehrsmittel
+     * 10)
+     * <p>
+     * Äquivalenzklasse: Kein Ticket für das Verkehrsmittel
+     * <p>
+     * Ausgangszustand:
+     * <br>
+     * Aktueller eigener Standort: Hauptbahnhof Bielefeld
+     *
+     * Benutztes Verkehrsmittel : tram
+     *
+     * Aktuelle Ticketanzahl: (Bus:2, Tram:0, eScooter:8)
+     * <p>
+     * Zug: (Zielort: Hauptbahnhof Bielefeld, Jahnplatz, Verkehrsmittel: Bus)
+     * <p>
+     * Erwartete Exception: ZeroTicketException
      */
     @Test
     public void testZeroTicketExceptionThrown() {
         try {
-            StartActivity.validateMove(
+            gameModel.validateMove(
                     currentPOI,
                     destinationTram,
                     TRAM,
                     player
             );
-            fail("Expected ZeroTicketException when detective has no scooter tickets.");
+            fail("Expected ZeroTicketException when detective has no tram tickets.");
         } catch (ZeroTicketException e) {
             assertTrue(true);
         } catch (Exception | InvalidConnectionException e) {
